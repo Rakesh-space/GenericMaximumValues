723ce400d13a7fd6cbc8dc523b767295ffaa776d
Rakesh-space <rakeshsmusale03@gmail.com>
2021-10-29 14:45:34 +0530

    Adding UC4 Extend max method

--- GenericMaximumValues/Program.cs
+++ GenericMaximumValues/Program.cs
@@ -6,14 +6,14 @@ namespace GenericMaximumValues
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("\n UC3MaximumValueGeneric class:");
-            Console.WriteLine("\n Enter the frirs String:");
-            string first = Convert.ToString(Console.ReadLine());
-            Console.WriteLine("\n Enter the Secound String:");
-            string secound = Convert.ToString(Console.ReadLine());
-            Console.WriteLine("\n Enter the third String:");
-            string third = Convert.ToString(Console.ReadLine());
-            Console.WriteLine("\n Maximum vlaue {0} out of 3 Variable.", UC3MaxStringThree.maxValue(first, secound, third)); 
+            Console.WriteLine("\n UC4MaximumValueGeneric class:");
+            Console.WriteLine("\n Enter the frirst Value:");
+            int first = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("\n Enter the Secound Value:");
+            int secound = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("\n Enter the third Value:");
+            int third = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("\n Maximum vlaue {0} out of 3 Variable.", UC4ExtendMaxMethod.maxMethod(first, secound, third)); */
         }
     }
 }
---
+++ GenericMaximumValues/UC4ExtendMaxMethod.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericMaximumValues
+{
+    class UC4ExtendMaxMethod
+    {
+        public static int maxMethod(int first, int secound, int third)
+        {
+            if (first > secound && first > third)
+            {
+                return first;
+            }
+            else if (secound > first && secound > third)
+            {
+                return secound;
+            }
+            else if (third > first && third > secound)
+            {
+                return third;
+            }
+            return first;
+        }
+    }
+}
