421f90a18a63a57e3f1a7612816332de07c51e74
Rakesh-space <rakeshsmusale03@gmail.com>
2021-10-29 18:49:52 +0530

    Addint UC1 Max Integer three of one

---
+++ GenericMaximumValues/UC1MaxIntThree.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace GenericMaximumValues
+{
+    class UC1MaxIntThree
+    {
+        public static int maxValue(int firstValue, int secoundValue, int thirdValue)
+        {
+            if (firstValue.CompareTo(secoundValue) > 0 && firstValue.CompareTo(thirdValue) > 0)
+            {
+                return firstValue;
+            }
+            else if (secoundValue.CompareTo(firstValue) > 0 && secoundValue.CompareTo(thirdValue) > 0)
+            {
+                return secoundValue;
+            }
+            else if (thirdValue.CompareTo(firstValue) > 0 && thirdValue.CompareTo(secoundValue) > 0)
+            {
+                return thirdValue;
+            }
+            return firstValue;
+        }
+    }
+}
