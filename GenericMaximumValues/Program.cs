ed3f02473cf02f3296adb6718701765379b57a37
Rakesh-space <rakeshsmusale03@gmail.com>
2021-10-29 12:42:27 +0530

    Adding Welcome message in generic

---
+++ GenericMaximumValues/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GenericMaximumValues
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\n Welcome to Generic Maximim Value..");
+        }
+    }
+}
