ee44f3a526acfa6bca2dcfa1fd7453c0071d9987
Rakesh-space <rakeshsmusale03@gmail.com>
2021-10-29 14:49:05 +0530

    Adding UC5 Generic method and class

--- GenericMaximumValues/Program.cs
+++ GenericMaximumValues/Program.cs
@@ -6,14 +6,10 @@ namespace GenericMaximumValues
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("\n UC4MaximumValueGeneric class:");
-            Console.WriteLine("\n Enter the frirst Value:");
-            int first = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("\n Enter the Secound Value:");
-            int secound = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("\n Enter the third Value:");
-            int third = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("\n Maximum vlaue {0} out of 3 Variable.", UC4ExtendMaxMethod.maxMethod(first, secound, third)); */
+            Console.WriteLine("\n UC5MaximumValueGeneric class:");
+            int[] arr = { 30, 2, 6 };
+            UC5MaximumValueGeneric<int> gen = new UC5MaximumValueGeneric<int>(arr);
+            gen.PrintMaxValue();
         }
     }
 }
---
+++ GenericMaximumValues/UC5MaximumValueGeneric.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericMaximumValues
+{
+    class UC5MaximumValueGeneric<T> where T : IComparable //declare the generic class here
+    {
+        T[] arrName;  //Genric Array declaration
+        public UC5MaximumValueGeneric(T[] arrName)  //constructor to initialize to generic array
+        {
+            this.arrName = arrName;
+        }
+
+        public T[] sortMethod(T[] values)  //user define generic method by using Array.Sort() method
+        {
+            Array.Sort(values);  //here Array.Sort() method
+            return values;
+        }
+        public T MaxValue(params T[] ar) // geting the element of generic array
+        {
+            var sortValue = sortMethod(ar);
+            return sortValue[^1];  // find out the max possition value
+        }
+
+        public T MaxMethod() //max value assgined to the generic array
+        {
+            var max = MaxValue(this.arrName);
+            return max;  //returned the max value
+        }
+
+        public void PrintMaxValue()   //Print the maximum value
+        {
+            var max = MaxValue(this.arrName);
+            Console.WriteLine("\n Maximum Values is:" + max);
+        }
+
+    }
+}
